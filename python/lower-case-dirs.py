3fac5af800eb7e57a21b3c40e0fcfee47e9d4dc2
Till <tf@homemadecode.de>
2023-07-05 10:47:30 +0200

    gpt version of lowercasing, thanks. i hate it

---
+++ python/lower-case-dirs.py
@@ -0,0 +1,29 @@
+import os
+import click
+
+def lowercase_directories(path):
+    for root, dirs, files in os.walk(path):
+        for directory in dirs:
+            original_name = os.path.join(root, directory)
+            lowercase_name = os.path.join(root, directory.lower())
+
+            if original_name != lowercase_name:
+                if os.path.exists(lowercase_name):
+                    suffix = 1
+                    while os.path.exists(lowercase_name + "_" + str(suffix)):
+                        suffix += 1
+                    lowercase_name += "_" + str(suffix)
+
+                os.rename(original_name, lowercase_name)
+                click.echo(f"Renamed: {original_name} -> {lowercase_name}")
+
+@click.command()
+@click.argument('directory_path', type=click.Path(exists=True))
+def lowercase_cli(directory_path):
+    """
+    Lowercase the names of all directories in the specified DIRECTORY_PATH.
+    """
+    lowercase_directories(directory_path)
+
+if __name__ == "__main__":
+    lowercase_cli()
