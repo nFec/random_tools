9d670966839e58bae70daa52ac90553047bb791b
Till <tf@homemadecode.de>
2023-07-05 09:41:57 +0200

    now it actually works, thanks chatgpt..

--- zsh/diff_dirs_l2r.zsh
+++ zsh/diff_dirs_l2r.zsh
@@ -3,11 +3,17 @@
 dir1=$1
 dir2=$2
 
-# Define the pattern for files to be ignored
-ignore_pattern='.DS_Store|Thumbs.db|.localized|desktop.ini|.TemporaryItems|.Spotlight-V100|.Trashes|.Trash'
+# Define the patterns for files and directories to be ignored
+ignore_patterns=("*.DS_Store" "*/.localized" "*/Thumbs.db" "*/desktop.ini" "*/.TemporaryItems" "*/.Spotlight-V100" "*/.Trashes" "*/.Trash-*")
 
-# Run diff command, excluding files matching the ignore pattern
-diff_output=$(diff -rq --exclude=$ignore_pattern "$dir1" "$dir2" | grep -E '^Only in '"$dir1" | cut -d: -f2-)
+# Generate the --exclude parameters for the diff command
+exclude_params=()
+for pattern in "${ignore_patterns[@]}"; do
+  exclude_params+=("--exclude=$pattern")
+done
+
+# Run diff command, excluding files and directories matching the ignore patterns
+diff_output=$(diff -rq "${exclude_params[@]}" "$dir1" "$dir2" | grep -E '^Only in '"$dir1" | cut -d: -f2-)
 
 if [[ -z $diff_output ]]; then
   echo "No left-to-right differences found."
